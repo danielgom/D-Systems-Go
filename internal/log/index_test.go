6e3463420805484689b21d2557bf558d4e471ccb
DANIEL GOMEZ <dgomez@perceptyx.com>
2022-08-07 17:32:22 -0500

    Chap 3 -- Writing the segment and segment tests

---
+++ internal/log/index_test.go
@@ -0,0 +1,57 @@
+package log
+
+import (
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
+
+func TestIndex(t *testing.T) {
+	f, err := os.CreateTemp(os.TempDir(), "index_test")
+	require.NoError(t, err)
+	defer func() {
+		err = os.Remove(f.Name())
+		require.NoError(t, err)
+	}()
+
+	c := Config{}
+	c.Segment.MaxIndexBytes = 1024
+	idx, err := newIndex(f, c)
+	require.NoError(t, err)
+	_, _, err = idx.Read(0)
+	require.Error(t, err)
+	require.Equal(t, f.Name(), idx.Name())
+
+	entries := []struct {
+		Off uint32
+		Pos uint64
+	}{
+		{Off: 0, Pos: 0},
+		{Off: 1, Pos: 10},
+	}
+
+	for _, want := range entries {
+		err = idx.Write(want.Off, want.Pos)
+		require.NoError(t, err)
+
+		_, pos, err := idx.Read(int64(want.Off))
+		require.NoError(t, err)
+		require.Equal(t, want.Pos, pos)
+	}
+
+	_, _, err = idx.Read(2)
+	require.EqualError(t, err, "EOF")
+	_ = idx.Close()
+
+	f, err = os.OpenFile(f.Name(), os.O_RDWR, 0600)
+	require.NoError(t, err)
+
+	idx, err = newIndex(f, c)
+	require.NoError(t, err)
+
+	off, pos, err := idx.Read(-1)
+	require.NoError(t, err)
+	require.Equal(t, uint32(1), off)
+	require.Equal(t, entries[1].Pos, pos)
+
+}
---
+++ internal/log/store_test.go
@@ -0,0 +1,71 @@
+package log
+
+import (
+	"github.com/stretchr/testify/require"
+	"log"
+	"os"
+	"testing"
+)
+
+var (
+	write      = []byte("hello world")
+	width      = uint64(len(write)) + lenWidth
+	readWrites = uint64(10)
+)
+
+func TestStoreAppendRead(t *testing.T) {
+	f, err := os.CreateTemp("", "store_append_read_test")
+	require.NoError(t, err)
+
+	defer func(name string) {
+		err = os.Remove(name)
+		if err != nil {
+			log.Fatalln("error while trying to close the test file store", err)
+		}
+	}(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+	testAppend(t, s)
+	testRead(t, s)
+	testReadAt(t, s)
+}
+
+func testAppend(t *testing.T, s *store) {
+	t.Helper()
+	for i := uint64(1); i < readWrites; i++ {
+		n, pos, err := s.Append(write)
+		require.NoError(t, err)
+		require.Equal(t, pos+n, width*i)
+	}
+}
+
+func testRead(t *testing.T, s *store) {
+	t.Helper()
+	var pos uint64
+	for i := uint64(1); i < readWrites; i++ {
+		read, err := s.Read(pos)
+		require.Nil(t, err)
+		require.Equal(t, write, read)
+		pos += width
+	}
+}
+
+func testReadAt(t *testing.T, s *store) {
+	t.Helper()
+	for i, off := uint64(1), int64(0); i < 4; i++ {
+		b := make([]byte, lenWidth)
+		n, err := s.ReadAt(b, off)
+		require.Nil(t, err)
+		require.Equal(t, lenWidth, n)
+		off += int64(n)
+
+		size := enc.Uint64(b)
+		b = make([]byte, size)
+		n, err = s.ReadAt(b, off)
+		require.Nil(t, err)
+		require.Equal(t, write, b)
+		require.Equal(t, int(size), n)
+		off += int64(n)
+	}
+}
